e6356e73962658571835a23a33443c11d1114a7b
Ganesh Sivadasan <ganeshsivadasan18@gmail.com>
2026-04-12 11:43:16 -0400

    feat: add CV camera reading via ZXing
    Uses OpenCV to open the webcam and has ZXing to read barcode

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,39 @@
 #include <fmt/core.h>
+#include <opencv2/opencv.hpp>
+#include <ZXing/ReadBarcode.h>
 
 int main()
 {
-    fmt::print("Hello World!\n");
+    cv::VideoCapture cap(0);
+    if (!cap.isOpened()) {
+        fmt::print("Error: Could not open camera\n");
+        return -1;
+    }
+
+    cv::Mat frame, grayframe;
+    while (true){
+        cap>>frame;
+        if(frame.empty()) break;
+        cv::cvtColor(frame, grayframe, cv::COLOR_BGR2GRAY);
+
+        cv::imshow("Video", grayframe);
+
+        ZXing::ImageView image(
+            grayframe.data,
+            grayframe.cols,
+            grayframe.rows,
+            ZXing::ImageFormat::Lum,
+            grayframe.step
+        );
+
+        auto results = ZXing::ReadBarcode(image);
+        if (results.isValid()){
+            fmt::print(results.text());
+            break;
+        }
+
+        if(cv::waitKey(10) == 27) break; 
+    }
+    cap.release();
     return 0;
 }
